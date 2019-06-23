162b98d7a97623de27a80718c7418a4fe6ed8672
Jose Albizures <albizures3601@gmail.com>
2019-06-22 19:17:18 -0600

    feat(layout): add more layout structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,55 @@ import { Layout, Container } from 'react-dynamic-layout';
 const IndexPage = () => (
   <Layout type={Layout.COLUMN}>
     <Container isFixedSize={true} initialSize={25}>
-      <label>Top</label>
+      <button>save</button>
+      <button>new</button>
+      <button>open</button>
     </Container>
     <Container>
       <Layout type={Layout.ROW}>
         <Container initialSize="15%">
-          <label>Left</label>
+          <label>Palette</label>
         </Container>
-        <Container initialSize="70%">
-          <label>Center</label>
-        </Container>
-        <Container initialSize="15%">
-          <label>Right</label>
+        <Container initialSize="85%">
+          <Layout type={Layout.COLUMN}>
+            <Container>
+              <Layout type={Layout.ROW}>
+                <Container isFixedSize={true}>
+                  <Layout type={Layout.COLUMN}>
+                    <Container isFixedSize={true} initialSize={25}>
+                      <label>Tabs</label>
+                    </Container>
+                    <Container>
+                      <canvas className="background" />
+                      <canvas className="main" />
+                      <canvas className="preview" />
+                      <canvas className="mask" />
+                    </Container>
+                  </Layout>
+                </Container>
+                <Container initialSize={60}>
+                  <button>pencil</button>
+                  <button>eraser</button>
+                  <button>pick</button>
+                  <button>bucket</button>
+                  <button>line</button>
+                  <button>rect</button>
+                </Container>
+              </Layout>
+            </Container>
+            <Container initialSize="30%">
+              {({ dimensions }) => (
+                <Layout type={Layout.ROW}>
+                  <Container isFixedSize={true}>
+                    <label>Frames and Layers</label>
+                  </Container>
+                  <Container initialSize={dimensions.height}>
+                    <canvas className="preview" />
+                  </Container>
+                </Layout>
+              )}
+            </Container>
+          </Layout>
         </Container>
       </Layout>
     </Container>
