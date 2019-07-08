1806c9f50818b946d3912f4dcf468a857faa475b
Jose Albizures <albizures3601@gmail.com>
2019-07-07 23:22:46 -0600

    feat(utils): add several util funtions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,22 @@
 let transparentPattern: HTMLCanvasElement;
 
+declare global {
+  interface CanvasRenderingContext2D {
+    msImageSmoothingEnabled: boolean;
+    mozImageSmoothingEnabled: boolean;
+  }
+}
+
+const imageSmoothing = (context: CanvasRenderingContext2D, value: boolean) => {
+  context.imageSmoothingEnabled = value;
+  context.mozImageSmoothingEnabled = value;
+  context.msImageSmoothingEnabled = value;
+};
+
+const imageSmoothingDisabled = (context: CanvasRenderingContext2D) => {
+  imageSmoothing(context, false);
+};
+
 const getTransparentPattern = () => {
   if (transparentPattern) {
     return transparentPattern;
@@ -27,4 +44,63 @@ const clean = (canvas: HTMLCanvasElement) => {
   return canvas;
 };
 
-export { getTransparentPattern, clean };
+interface Contexts {
+  [key: string]: CanvasRenderingContext2D;
+}
+
+let counter = 0;
+const mapReferences = new WeakMap<object, string>();
+const getIdByReference = (obj: object): string => {
+  if (mapReferences.has(obj)) {
+    return mapReferences.get(obj);
+  }
+  const id = counter.toString();
+  mapReferences.set(obj, id);
+  counter++;
+  return id;
+};
+
+const contexts: Contexts = {};
+
+const getContext = (id: string) => contexts[id];
+const addContext = (context: CanvasRenderingContext2D): string => {
+  const id = getIdByReference(context);
+  if (contexts[id]) {
+    throw new Error('`' + id + '` context already exists');
+  }
+  if (context.canvas.width === 0) {
+    throw new Error('Width cannot be 0 of a canvas');
+  }
+  if (context.canvas.height === 0) {
+    throw new Error('Height cannot be 0 of a canvas');
+  }
+  contexts[id] = context;
+
+  return id;
+};
+
+type NewContext = {
+  context: CanvasRenderingContext2D;
+  width: number;
+  height: number;
+};
+
+const getNewContext = ({ context, width, height }: NewContext) => {
+  var newContext = context || document.createElement('canvas').getContext('2d');
+  if (!context) {
+    newContext.canvas.width = width;
+    newContext.canvas.height = height;
+  }
+  return newContext;
+};
+
+export {
+  getTransparentPattern,
+  clean,
+  imageSmoothing,
+  imageSmoothingDisabled,
+  getContext,
+  addContext,
+  getNewContext,
+  getIdByReference,
+};
