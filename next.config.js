fe36c98d40a470467ce7945ff281bf4c8808a0a3
Jose Albizures <albizures3601@gmail.com>
2019-07-16 20:11:50 -0600

    chore(next): update next version

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-const withTypescript = require('@zeit/next-typescript');
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS(
-  withTypescript({
-    target: 'serverless',
-    webpack: (config, { webpack }) => {
-      config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-      return config;
-    },
-    webpackDevMiddleware: (config) => config,
-  }),
-);
+module.exports = withCSS({
+  target: 'serverless',
+  webpack: (config, { webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+    return config;
+  },
+  webpackDevMiddleware: (config) => config,
+});
