64367555ad0893e4b9f2f41843d3eee885348691
Jose Albizures <albizures3601@gmail.com>
2019-07-18 19:45:54 -0600

    fix(next): revert next version

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const withTypescript = require('@zeit/next-typescript');
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS({
-  target: 'serverless',
-  webpack: (config, { webpack }) => {
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-    return config;
-  },
-  webpackDevMiddleware: (config) => config,
-});
+module.exports = withCSS(
+  withTypescript({
+    target: 'serverless',
+    webpack: (config, { webpack }) => {
+      config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+      return config;
+    },
+    webpackDevMiddleware: (config) => config,
+  }),
+);
