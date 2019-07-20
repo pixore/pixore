0b98064ef31d65f8cf70d7386e9bf77f1acdbdfc
Jose Albizures <albizures3601@gmail.com>
2019-07-20 12:45:41 -0600

    feat(bootstrap): create bootstrap component

---
+++ src/components/Bootstrap.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+import { useSprite } from '../contexts/Sprite';
+import { useSpritesActions, useSprites } from '../contexts/Sprites';
+import { useSpriteActions } from '../contexts/Sprite';
+import { useArtboards, useArtboardsActions } from '../contexts/Artboards';
+import { useArtboard } from '../contexts/Artboard';
+
+interface PropTypes {
+  children: React.ReactNode;
+}
+
+const Bootstrap: React.FC<PropTypes> = (props) => {
+  const { children } = props;
+  const { addNewLayerToSprite, addNewFrameToSprite } = useSpriteActions();
+  const { addSprite } = useSpritesActions();
+  const { addArtboard } = useArtboardsActions();
+
+  const sprite = useSprite();
+  const sprites = useSprites();
+  const artboards = useArtboards();
+  const artboard = useArtboard();
+
+  if (!sprite) {
+    const spriteIds = Object.keys(sprites);
+    if (spriteIds.length === 0) {
+      addSprite({
+        id: '1',
+        name: 'New Sprite',
+        width: 50,
+        height: 50,
+        layers: [],
+        frames: [],
+      });
+    }
+
+    return null;
+  }
+
+  if (sprite.layers.length === 0) {
+    addNewLayerToSprite({
+      name: 'First Layer',
+    });
+
+    return null;
+  }
+
+  if (sprite.frames.length === 0) {
+    addNewFrameToSprite();
+
+    return null;
+  }
+
+  if (!artboard) {
+    const { id, frames, layers } = sprite;
+    const artboardIds = Object.keys(artboards);
+    if (artboardIds.length === 0) {
+      addArtboard({
+        id,
+        scale: 1,
+        frame: frames[0],
+        layer: layers[0],
+        y: 0,
+        x: 0,
+      });
+    }
+
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default Bootstrap;
