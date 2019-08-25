b9973586260cdcb2e116505eefd17084ebf6c8d3
Jose Albizures <albizures3601@gmail.com>
2019-08-25 11:42:10 -0600

    feat(tools): create tool utils
    create addPreview
    create addPanning

--- src/tools/utils.ts
+++ src/tools/utils.ts
@@ -1,22 +1,103 @@
-import { ListenerContextRef } from './types';
-import { clean } from '../utils';
-import { calculatePosition, validCord } from '../utils/canvas';
-import { paintPreview } from '../utils/paint';
+import { ContextRef } from './types';
+import { Key } from '../contexts/Modifiers';
+import { getModifierState } from '../utils/keyboard';
+import { manageEvents as $ } from '../utils/dom/events';
+import Vector from '../utils/vector';
+import { round2, clean } from '../utils';
+import { calculatePosition } from '../utils/canvas';
 
-const createOnMouseMovePreview = (listenerContextRef: ListenerContextRef) => (
-  event: MouseEvent,
-) => {
-  const { artboard, sprite, context, canvas } = listenerContextRef.current;
-  event.preventDefault();
-  const { clientX, clientY } = event;
-  clean(canvas);
-  const cord = calculatePosition(artboard, clientX, clientY);
+type RemovePanning = () => void;
+type RemovePreview = () => void;
 
-  if (validCord(sprite, cord)) {
-    paintPreview(cord, context, artboard);
-  } else {
-    clean(canvas);
-  }
+const addPreview = (contextRef: ContextRef): RemovePreview => {
+  const $window = $(window);
+
+  const paint = (cord: Vector) => {
+    const { x, y } = cord;
+    const { artboard, previewContext } = contextRef.current;
+    const { scale } = artboard;
+    const previewX = round2(x * scale + artboard.x);
+    const previewY = round2(y * scale + artboard.y);
+
+    clean(previewContext.canvas);
+
+    previewContext.fillStyle = 'red';
+    previewContext.fillRect(previewX, previewY, scale, scale);
+  };
+
+  const onMouseMove = (event: MouseEvent) => {
+    const { artboard } = contextRef.current;
+    const { clientX, clientY } = event;
+    const cord = calculatePosition(artboard, clientX, clientY);
+
+    paint(cord);
+  };
+
+  $window.on('mousemove', onMouseMove);
+
+  return () => {
+    $window.off('mousemove', onMouseMove);
+  };
 };
 
-export { createOnMouseMovePreview };
+const addPanning = (contextRef: ContextRef): RemovePanning => {
+  const { previewContext } = contextRef.current;
+  const $window = $(window);
+  const $previewCanvas = $(previewContext.canvas);
+
+  const saveLastDrag = (lastDrag: Vector) => {
+    contextRef.current.lastDrag = lastDrag;
+  };
+
+  const onMouseMovePanning = (event: MouseEvent) => {
+    const { artboard, artboardActions, lastDrag } = contextRef.current;
+
+    const { changePosition } = artboardActions;
+    const { clientX, clientY } = event;
+    const currentDrag: Vector = { x: clientX, y: clientY };
+
+    const dragDiff = lastDrag
+      ? Vector.getDelta(currentDrag, lastDrag)
+      : { x: 0, y: 0 };
+
+    event.preventDefault();
+
+    changePosition({
+      x: artboard.x + dragDiff.x,
+      y: artboard.y + dragDiff.y,
+      scale: artboard.scale,
+    });
+
+    saveLastDrag(currentDrag);
+  };
+
+  const onMouseUpPanning = () => {
+    saveLastDrag(undefined);
+    $window
+      .off('mousemove', onMouseMovePanning)
+      .off('mouseup', onMouseUpPanning);
+  };
+
+  const onMouseDown = (event: MouseEvent) => {
+    const { clientX, clientY } = event;
+    const isPanning = getModifierState(Key.Spacebar);
+
+    saveLastDrag({ x: clientX, y: clientY });
+
+    if (isPanning) {
+      $window
+        .on('mousemove', onMouseMovePanning)
+        .on('mouseup', onMouseUpPanning);
+    }
+  };
+
+  $previewCanvas.on('mousedown', onMouseDown);
+  return () => {
+    $previewCanvas.off('mousedown', onMouseDown);
+    $window
+      .off('mousemove', onMouseMovePanning)
+      .off('mouseup', onMouseUpPanning);
+  };
+};
+
+export { addPanning, addPreview };
